a1a930c77dbc0850d27c6bf07507145045840d09
uvreddymsm <uvreddymsm@gmail.com>
2021-07-11 15:17:57 +0530

    Reset Head

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -3,6 +3,8 @@ package com.company;
 public class Test {
 
     System.out.println("This Is Yuvaraju From Test Calss");
+    System.out.println("First Statement");
+    System.out.println("Second Statement");
 }
 
 
